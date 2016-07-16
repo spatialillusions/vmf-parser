16336d59f8962c5614e788edf4496b509112603e
Måns Beckman <mans.beckman@spatialillusions.com>
2016-07-16 15:02:03 +0200

    changed casing

--- dev/object-structure.js
+++ dev/object-structure.js
@@ -1,132 +1,130 @@
 var header = {
-	VERSION :4 ,
-	DATA COMPRESSION TYPE : 2,
-	ORIGINATOR ADDRESS GROUP : {
-		URN : 24,
-		UNIT NAME :448 
+	Version :4 ,
+	DataCompressionType : 2,
+	OriginatorAddressGroup : {
+		Urn : 24,
+		UnitName :448 
 	},
-	RECIPIENT ADDRESS GROUP : [
+	RecipientAddressGroup : [
 		{
-			URN : 24,
-			UNIT NAME : 448,
+			Urn : 24,
+			UnitName : 448,
 		}
-		...
-	]
-	INFORMATION ADDRESS GROUP : [
+	],
+	InformationAddressGroup : [
 		{
-			URN : 24,
-			UNIT NAME : 448,
+			Urn : 24,
+			UnitName : 448,
 		}
-		...
-	]
-	HEADER SIZE : 16,
-	FUTURE USE 1 : 1,
-	FUTURE USE 2 : 1,
-	FUTURE USE 3 : 1,
-	FUTURE USE 4 : 1,
-	FUTURE USE 5 : 1,
-	MESSAGE HANDLING GROUP : [
+	],
+	HeaderSize : 16,
+	FutureUse1 : 1,
+	FutureUse2 : 1,
+	FutureUse3 : 1,
+	FutureUse4 : 1,
+	FutureUse5 : 1,
+	MessageHandlingGroup : [
 		{
-			UMF : 4,
-			MESSAGE STANDARD VERSION : 4,
-			VMF MESSAGE IDENTIFICATION GROUP : {
-				FAD : 4,
-				MESSAGE NUMBER : 7,
-				MESSAGE SUBTYPE : 7,	
+			Umf : 4,
+			MessageStandardVersion : 4,
+			VmfMessageIdentificationGroup : {
+				Fad : 4,
+				MessageNumber : 7,
+				MessageSubtype : 7,	
 			}
 		},
-		FILE NAME : 448,
-		MESSAGE SIZE : 20,
-		OPERATION INDICATOR : 2,
-		RETRANSMIT INDICATOR : 1,
-		MESSAGE PRECEDENCE CODE : 3,
-		SECURITY CLASSIFICATION : 2,
-		CONTROL/RELEASE MARKING : [9],
-		ORIGINATOR DTG : {
-			YEAR : 7,
-			MONTH : 4,
-			DAY : 5,
-			HOUR : 5,
-			MINUTE : 6,
-			SECOND :6 ,
-			DTG EXTENSION : 12
+		FileName : 448,
+		MessageSize : 20,
+		OperationIndicator : 2,
+		RetransmitIndicator : 1,
+		MessagePrecedenceCode : 3,
+		SecurityClassification : 2,
+		ControlReleaseMarking : [9],
+		OriginatorDtg : {
+			Year : 7,
+			Month : 4,
+			Day : 5,
+			Hour : 5,
+			Minute : 6,
+			Second :6 ,
+			DtgExtension : 12
 		},
-		PERISHABILITY DTG : {
-			YEAR : 7,
-			MONTH : 4,
-			DAY : 5,
-			HOUR : 5,
-			MINUTE : 6,
-			SECOND : 6
+		PerishabilityDtg : {
+			Year : 7,
+			Month : 4,
+			Day : 5,
+			Hour : 5,
+			Minute : 6,
+			Second : 6
 		},
-		ACKNOWLEDGMENT REQUEST GROUP : {
-			MACHINE ACKNOWLEDGE REQUEST INDICATOR : 1,
-			OPERATOR ACKNOWLEDGE REQUEST INDICATOR : 1,
-			OPERATOR REPLY REQUEST INDICATOR : 1
+		AcknowledgmentRequestGroup : {
+			MachineAcknowledgeRequestIndicator : 1,
+			OperatorAcknowledgeRequestIndicator : 1,
+			OperatorReplyRequestIndicator : 1
 		},
-		RESPONSE DATA GROUP : {
-			YEAR : 7,
-			MONTH : 4,
-			DAY : 5,
-			HOUR : 5,
-			MINUTE : 6,
-			SECOND : 6,
-			DTG EXTENSION : 12,
-			RESPONSE TO ACKNOWLEDGE REQUEST	: 3,
-			CANTCO REASON CODE : 3,
-			CANTPRO REASON CODE : 6,
-			REPLY AMPLIFICATION : 350
+		ResponseDataGroup : {
+			Year : 7,
+			Month : 4,
+			Day : 5,
+			Hour : 5,
+			Minute : 6,
+			Second : 6,
+			DtgExtension : 12,
+			ResponseToAcknowledgeRequest	: 3,
+			CantcoReasonCode : 3,
+			CantproReasonCode : 6,
+			ReplyAmplification : 350
 		},
-		REFERENCE MESSAGE DATA GROUP : [
+		ReferenceMessageDataGroup : [
 			{
-				URN : 24,
-				UNIT NAME : 448,
-				YEAR : 7,
-				MONTH : 4,
-				DAY : 5,
-				HOUR : 5,
-				MINUTE : 6,
-				SECOND : 6,
-				DTG EXTENSION : 12
+				Urn : 24,
+				UnitName : 448,
+				Year : 7,
+				Month : 4,
+				Day : 5,
+				Hour : 5,
+				Minute : 6,
+				Second : 6,
+				DtgExtension : 12
 			}
 		],
-		FUTURE USE 6 : 1,
-		FUTURE USE 7 : 1,
-		FUTURE USE 8 : 1,
-		FUTURE USE 9 : 1,
-		FUTURE USE 10 : 1,
-		MESSAGE SECURITY GROUP : {
-			SECURITY PARAMETERS INFORMATION : 4,
-			KEYING MATERIAL GROUP : {
-				LENGTH : 3,
-				KEYING MATERIAL ID : 64
+		FutureUse6 : 1,
+		FutureUse7 : 1,
+		FutureUse8 : 1,
+		FutureUse9 : 1,
+		FutureUse10 : 1,
+		MessageSecurityGroup : {
+			SecurityParametersInformation : 4,
+			KeyingMaterialGroup : {
+				Length : 3,
+				KeyingMaterialId : 64
 			},
-			CRYPTOGRAPHIC INITIALIZATION GROUP : {
-				LENGTH : 4,
-				CRYPTOGRAPHIC INITIALIZATION : 1024,
+			CryptographicInitializationGroup : {
+				Length : 4,
+				CryptographicInitialization : 1024,
 			},
-			KEY TOKEN GROUP : {
-				LENGTH : 8,
-				KEY TOKEN : [16384]
+			KeyTokenGroup : {
+				Length : 8,
+				KeyToken : [16384]
 			}
-			AUTHENTICATION (A) GROUP :{
-				LENGTH : 7,
-				DIGITAL SIGNATURE : 8192
+			AuthenticationAGroup :{
+				Length : 7,
+				DigitalSignature : 8192
 			},
-			AUTHENTICATION (B) GROUP : {
-				LENGTH : 7,
-				DIGITAL SIGNATURE : 8192
+			AuthenticationBGroup : {
+				Length : 7,
+				DigitalSignature : 8192
 			},
-			SIGNED ACKNOWLEDGE REQUEST INDICATOR : 1,
-			MESSAGE SECURITY PADDING GROUP : {
-				LENGTH : 8,
-				MESSAGE SECURITY PADDING : 2040
+			SignedAcknowledgeRequestIndicator : 1,
+			MessageSecurityPaddingGroup : {
+				Length : 8,
+				MessageSecurityPadding : 2040
 			}
 		}
 	],
-	FUTURE USE 11 : 1,
-	FUTURE USE 12 : 1,
-	FUTURE USE 13 : 1,
-	FUTURE USE 14 : 1,
-	FUTURE USE 15 : 1
+	FutureUse11 : 1,
+	FutureUse12 : 1,
+	FutureUse13 : 1,
+	FutureUse14 : 1,
+	FutureUse15 : 1
 }
