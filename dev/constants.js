8f59d9ae8c81cb50f54c1e5ced6c5742c574bb9b
Måns Beckman <mans.beckman@spatialillusions.com>
2016-07-16 14:10:41 +0200

    cleaning things up

---
+++ dev/constants.js
@@ -0,0 +1,80 @@
+var VersionCodes = [
+	'MIL-STD-2045-47001',
+	'MIL-STD-2045-47001B',
+	'MIL-STD-2045-47001C',
+	'MIL-STD-2045-47001D',
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	'Version Sent Not Implemented'
+];
+
+var DataCompressionTypeCodes = [
+	'Unix compress/uncompress',
+	'GZIP',
+	undefined,
+	undefined
+];
+
+var UMFcodes = [
+	'Link 16',
+	'Binary File',
+	'Variable Message Format (VMF)',
+	'National Imagery Transmission Format System (NITFS)',
+	'Redistributed Message (RDM)',
+	'United States Message Text Format (USMTF)',
+	'(DOI-103)',
+	'eXtensible Markup Language (XML) - Message Text Format (MTF)',
+	'eXtensible Markup Language (XML) - Variable Message Format (VMF)',
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+	undefined
+];
+
+var OperationIndicatorCodes = [
+	'Operation',
+	'Exercise',
+	'Simulation',
+	'Test'
+];
+
+var MessagePrecedenceCodes = [
+	'Routine',
+	'Priority',
+	'Immediate',
+	'Flash',
+	'Flash Override',
+	'CRITIC/ECP',
+	'Reserved',
+	'Reserved'
+];
+
+var SecurityClassificationCodes = [
+	'Unclassified',
+	'Confidential',
+	'Secret',
+	'Top secret'
+];
+
+var RCcodes = [
+	undefined,
+	'Machine Receipt',
+	'Cannot Process',
+	'Operator Acknowledge',
+	'Will Comply',
+	'Have Complied',
+	'Cannot Comply',
+	undefined
+];
